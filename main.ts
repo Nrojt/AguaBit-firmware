b0dedb50ff840c021c802a942e238277a62260e3
Tjorn <74906283+Nrojt@users.noreply.github.com>
2022-11-16 22:52:44 +0100

    Update main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,14 +1,35 @@
-input.onButtonPressed(Button.A, function () {
-    ph_value = Math.round(dstemp.celsius(DigitalPin.P0))
-})
-let ph_value = 0
-datalogger.setColumnTitles(
-"Slot1",
-"Slot2",
-"Slot3"
-)
-basic.forever(function () {
-    basic.showNumber(ph_value)
-    serial.writeNumber(25)
-    basic.pause(5000)
-})
+let slotInput = ""
+let state = 0
+let textInput = "B"
+let slot1 = "EM"
+let slot2 = "EM"
+let slot3 = "EM"
+let ph_value = 69.69
+let temp_value = 45.45
+while (state == 0) {
+    textInput = serial.readString()
+    if (textInput.indexOf("M") >= 0) {
+        state = 1
+        break;
+    } else if (textInput.indexOf("C") >= 0) {
+        state = 2
+        break;
+    }
+}
+while (state == 1) {
+    basic.showIcon(IconNames.Heart)
+    slotInput = serial.readString()
+    slot1 = "" + slotInput.charAt(0) + slotInput.charAt(1)
+    slot2 = "" + slotInput.charAt(2) + slotInput.charAt(3)
+    slot3 = "" + slotInput.charAt(4) + slotInput.charAt(5)
+    if (slot1 == "EM") {
+        serial.writeLine("" + 0)
+    } else if (slot1 == "PH") {
+        serial.writeLine("" + ph_value)
+    } else if (slot1 == "TP") {
+        serial.writeLine("" + temp_value)
+    }
+    pause(2000)
+state = 0
+    break;
+}
