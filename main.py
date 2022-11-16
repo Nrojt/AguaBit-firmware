2bb9ef295bb55fac30640efa311ccdc5f4152f9f
Tjorn <74906283+Nrojt@users.noreply.github.com>
2022-11-16 22:50:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-def on_button_pressed_a():
-    global ph_value
-    ph_value = Math.round(dstemp.celsius(DigitalPin.P0))
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-ph_value = 0
-datalogger.set_column_titles("Slot1", "Slot2", "Slot3")
-
-def on_forever():
-    basic.show_number(ph_value)
-    serial.write_number(25)
-    basic.pause(5000)
-basic.forever(on_forever)
+slotInput = ""
+state = 0
+textInput = "B"
+slot1 = "EM"
+slot2 = "EM"
+slot3 = "EM"
+ph_value = 69.69
+temp_value = 45.45
+while state == 0:
+    textInput = serial.read_string()
+    if textInput.index_of("M") >= 0:
+        state = 1
+        break
+    elif textInput.index_of("C") >= 0:
+        state = 2
+        break
+while state == 1:
+    basic.show_icon(IconNames.HEART)
+    slotInput = serial.read_string()
+    slot1 = "" + slotInput.char_at(0) + slotInput.char_at(1)
+    slot2 = "" + slotInput.char_at(2) + slotInput.char_at(3)
+    slot3 = "" + slotInput.char_at(4) + slotInput.char_at(5)
+    if slot1 == "EM":
+        serial.write_line("" + str(0))
+    elif slot1 == "PH":
+        serial.write_line("" + str(ph_value))
+    elif slot1 == "TP":
+        serial.write_line("" + str(temp_value))
+    pause(2000)
+    state = 0
+    break
