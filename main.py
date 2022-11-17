93a928728974082cad030cb3ec0cd103c498a3b9
Tjorn <74906283+Nrojt@users.noreply.github.com>
2022-11-17 10:30:39 +0100

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-slotInput = ""
+slotInput = "your mom"
 state = 0
 textInput = "B"
 slot1 = "EM"
@@ -6,26 +6,34 @@ slot2 = "EM"
 slot3 = "EM"
 ph_value = 69.69
 temp_value = 45.45
-while state == 0:
-    textInput = serial.read_string()
-    if textInput.index_of("M") >= 0:
-        state = 1
+
+def on_forever():
+    global textInput, state, slotInput, slot1, slot2, slot3
+    while state == 0:
+        basic.show_icon(IconNames.STICK_FIGURE)
+        textInput = serial.read_string()
+        if textInput.index_of("M") >= 0:
+            state = 1
+            break
+        elif textInput.index_of("C") >= 0:
+            state = 2
+            break
+    while state == 1:
+        basic.show_icon(IconNames.HEART)
+        slotInput = serial.read_string()
+        basic.show_string(slotInput)
+        slot1 = "" + slotInput.char_at(0) + slotInput.char_at(1)
+        slot2 = "" + slotInput.char_at(2) + slotInput.char_at(3)
+        slot3 = "" + slotInput.char_at(4) + slotInput.char_at(5)
+        if slot1 == "EM":
+            serial.write_line("" + str(0))
+        elif slot1 == "PH":
+            serial.write_line("" + str(ph_value))
+        elif slot1 == "TP":
+            serial.write_line("" + str(temp_value))
+        else:
+            serial.write_number(33.33)
+        pause(10000)
+        state = 0
         break
-    elif textInput.index_of("C") >= 0:
-        state = 2
-        break
-while state == 1:
-    basic.show_icon(IconNames.HEART)
-    slotInput = serial.read_string()
-    slot1 = "" + slotInput.char_at(0) + slotInput.char_at(1)
-    slot2 = "" + slotInput.char_at(2) + slotInput.char_at(3)
-    slot3 = "" + slotInput.char_at(4) + slotInput.char_at(5)
-    if slot1 == "EM":
-        serial.write_line("" + str(0))
-    elif slot1 == "PH":
-        serial.write_line("" + str(ph_value))
-    elif slot1 == "TP":
-        serial.write_line("" + str(temp_value))
-    pause(2000)
-    state = 0
-    break
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,35 +1,45 @@
-let slotInput = ""
 let state = 0
+let slotInput = "your mom"
 let textInput = "B"
 let slot1 = "EM"
 let slot2 = "EM"
 let slot3 = "EM"
 let ph_value = 69.69
 let temp_value = 45.45
-while (state == 0) {
-    textInput = serial.readString()
-    if (textInput.indexOf("M") >= 0) {
-        state = 1
-        break;
-    } else if (textInput.indexOf("C") >= 0) {
-        state = 2
-        break;
+basic.forever(function () {
+    while (state == 0) {
+        basic.showIcon(IconNames.StickFigure)
+        textInput = serial.readString()
+        if (textInput.indexOf("M") >= 0) {
+            state = 1
+            break;
+        } else if (textInput.indexOf("C") >= 0) {
+            state = 2
+            break;
+        }
     }
-}
-while (state == 1) {
-    basic.showIcon(IconNames.Heart)
-    slotInput = serial.readString()
-    slot1 = "" + slotInput.charAt(0) + slotInput.charAt(1)
-    slot2 = "" + slotInput.charAt(2) + slotInput.charAt(3)
-    slot3 = "" + slotInput.charAt(4) + slotInput.charAt(5)
-    if (slot1 == "EM") {
-        serial.writeLine("" + 0)
-    } else if (slot1 == "PH") {
-        serial.writeLine("" + ph_value)
-    } else if (slot1 == "TP") {
-        serial.writeLine("" + temp_value)
-    }
-    pause(2000)
+    while (state == 1) {
+        basic.showIcon(IconNames.Heart)
+        slotInput = serial.readString()
+        basic.showString(slotInput)
+        slot1 = "" + slotInput.charAt(0) + slotInput.charAt(1)
+        slot2 = "" + slotInput.charAt(2) + slotInput.charAt(3)
+        slot3 = "" + slotInput.charAt(4) + slotInput.charAt(5)
+        if (slot1 == "EM") {
+            serial.writeLine("" + 0)
+            basic.showIcon(IconNames.Skull)
+        } else if (slot1 == "PH") {
+            serial.writeLine("" + ph_value)
+            basic.showIcon(IconNames.Cow)
+        } else if (slot1 == "TP") {
+            serial.writeLine("" + temp_value)
+            basic.showIcon(IconNames.LeftTriangle)
+        } else {
+            serial.writeNumber(33.33)
+            basic.showIcon(IconNames.Ghost)
+        }
+        pause(10000)
 state = 0
-    break;
-}
+        break;
+    }
+})
