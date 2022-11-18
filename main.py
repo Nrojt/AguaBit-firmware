00b62550253a6554ff6d0b94b5a27e6c2b8becc0
Tjorn <74906283+Nrojt@users.noreply.github.com>
2022-11-18 12:01:34 +0100

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-slotInput = ""
 state = 0
 textInput = "B"
 slot1 = "AB"
@@ -8,42 +7,45 @@ ph_value = 69.69
 temp_value = 45.45
 
 def on_forever():
-    global textInput, state, slotInput, slot1, slot2, slot3
+    global textInput, state
     while state == 0:
         basic.show_icon(IconNames.STICK_FIGURE)
         textInput = serial.read_string()
-        if textInput.index_of("M") >= 0:
-            state = 1
-            break
-        elif textInput.index_of("C") >= 0:
+        if textInput.index_of("X") >= 0:
             state = 2
             break
+        elif textInput.index_of("Y") >= 0:
+            state = 1
+            break
     while state == 1:
-        basic.show_icon(IconNames.HEART)
-        slotInput = serial.read_string()
-        slot1 = "" + slotInput.char_at(0) + slotInput.char_at(1)
-        slot2 = "" + slotInput.char_at(2) + slotInput.char_at(3)
-        slot3 = "" + slotInput.char_at(4) + slotInput.char_at(5)
-        if slot1.char_at(0) == "E" or "P" or "T":
-            basic.show_icon(IconNames.YES)
-            serial.write_number(0)
-            if slot1 == "EM":
-                serial.write_line("" + str(0))
-            elif slot1 == "PH":
-                serial.write_line("" + str(ph_value))
-            elif slot1 == "TP":
-                serial.write_line("" + str(temp_value))
-            if slot2 == "EM":
-                serial.write_line("" + str(0))
-            elif slot2 == "PH":
-                serial.write_line("" + str(ph_value))
-            elif slot2 == "TP":
-                serial.write_line("" + str(temp_value))  
-            if slot3 == "EM":
-                serial.write_line("" + str(0))
-            elif slot3 == "PH":
-                serial.write_line("" + str(ph_value))
-            elif slot3 == "TP":
-                serial.write_line("" + str(temp_value))                              
-        pause(100)
+        basic.show_icon(IconNames.SMALL_HEART)
+        slot12 = "" + textInput.char_at(1) + textInput.char_at(2)
+        slot22 = "" + textInput.char_at(3) + textInput.char_at(4)
+        slot32 = "" + textInput.char_at(5) + textInput.char_at(6)
+        if slot12.char_at(0) == "E" or slot12.char_at(0) == "P" or slot12.char_at(0) == "T":
+            basic.show_string(slot12)
+            pause(200)
+            textInput = "B"
+            state = 0
+            break
+    while state == 2:
+        basic.show_icon(IconNames.HOUSE)
+        if slot12 == "EM":
+            serial.write_line("" + str(0))
+        elif slot12 == "PH":
+            serial.write_line("" + str(ph_value))
+        elif slot12 == "TP":
+            serial.write_line("" + str(temp_value))
+        if slot22 == "EM":
+            serial.write_line("" + str(0))
+        elif slot22 == "PH":
+            serial.write_line("" + str(ph_value))
+        elif slot22 == "TP":
+            serial.write_line("" + str(temp_value))
+        if slot32 == "EM":
+            serial.write_line("" + str(0))
+        elif slot32 == "PH":
+            serial.write_line("" + str(ph_value))
+        elif slot32 == "TP":
+            serial.write_line("" + str(temp_value))
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-let slotInput = ""
 let state = 0
 let textInput = "B"
 let slot1 = "AB"
@@ -7,54 +6,61 @@ let slot3 = "AB"
 let ph_value = 69.69
 let temp_value = 45.45
 basic.forever(function on_forever() {
+    let slot12: string;
+    let slot22: any;
+    let slot32: any;
     
     while (state == 0) {
         basic.showIcon(IconNames.StickFigure)
         textInput = serial.readString()
-        if (textInput.indexOf("M") >= 0) {
-            state = 1
-            break
-        } else if (textInput.indexOf("C") >= 0) {
+        if (textInput.indexOf("X") >= 0) {
             state = 2
+            break
+        } else if (textInput.indexOf("Y") >= 0) {
+            state = 1
             break
         }
         
     }
     while (state == 1) {
-        basic.showIcon(IconNames.Heart)
-        slotInput = serial.readString()
-        slot1 = "" + slotInput.charAt(0) + slotInput.charAt(1)
-        slot2 = "" + slotInput.charAt(2) + slotInput.charAt(3)
-        slot3 = "" + slotInput.charAt(4) + slotInput.charAt(5)
-        if (slot1.charAt(0) == "E" || "P" || "T") {
-            basic.showIcon(IconNames.Yes)
-            serial.writeNumber(0)
-            if (slot1 == "EM") {
-                serial.writeLine("" + ("" + 0))
-            } else if (slot1 == "PH") {
-                serial.writeLine("" + ("" + ph_value))
-            } else if (slot1 == "TP") {
-                serial.writeLine("" + ("" + temp_value))
-            }
-            
-            if (slot2 == "EM") {
-                serial.writeLine("" + ("" + 0))
-            } else if (slot2 == "PH") {
-                serial.writeLine("" + ("" + ph_value))
-            } else if (slot2 == "TP") {
-                serial.writeLine("" + ("" + temp_value))
-            }
-            
-            if (slot3 == "EM") {
-                serial.writeLine("" + ("" + 0))
-            } else if (slot3 == "PH") {
-                serial.writeLine("" + ("" + ph_value))
-            } else if (slot3 == "TP") {
-                serial.writeLine("" + ("" + temp_value))
-            }
-            
+        basic.showIcon(IconNames.SmallHeart)
+        slot12 = "" + textInput.charAt(1) + textInput.charAt(2)
+        slot22 = "" + textInput.charAt(3) + textInput.charAt(4)
+        slot32 = "" + textInput.charAt(5) + textInput.charAt(6)
+        if (slot12.charAt(0) == "E" || slot12.charAt(0) == "P" || slot12.charAt(0) == "T") {
+            basic.showString(slot12)
+            pause(200)
+            textInput = "B"
+            state = 0
+            break
         }
         
-        pause(100)
+    }
+    while (state == 2) {
+        basic.showIcon(IconNames.House)
+        if (slot12 == "EM") {
+            serial.writeLine("" + ("" + 0))
+        } else if (slot12 == "PH") {
+            serial.writeLine("" + ("" + ph_value))
+        } else if (slot12 == "TP") {
+            serial.writeLine("" + ("" + temp_value))
+        }
+        
+        if (slot22 == "EM") {
+            serial.writeLine("" + ("" + 0))
+        } else if (slot22 == "PH") {
+            serial.writeLine("" + ("" + ph_value))
+        } else if (slot22 == "TP") {
+            serial.writeLine("" + ("" + temp_value))
+        }
+        
+        if (slot32 == "EM") {
+            serial.writeLine("" + ("" + 0))
+        } else if (slot32 == "PH") {
+            serial.writeLine("" + ("" + ph_value))
+        } else if (slot32 == "TP") {
+            serial.writeLine("" + ("" + temp_value))
+        }
+        
     }
 })
